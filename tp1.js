b7e2071163ced680c99d00ae67718c73aa9c448d
robledo66 <111396707+robledo66@users.noreply.github.com>
2023-05-19 20:56:02 -0300

    Add files via upload

---
+++ tp1.js
@@ -0,0 +1,105 @@
+const result = document.querySelector('.result');
+const form = document.querySelector('.get-weather');
+const nameCity = document.querySelector('#city');
+const nameCountry = document.querySelector('#country');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (nameCity.value === '' || nameCountry.value === '') {
+        showError('Ambos campos son obligatorios...');
+        return;
+    }
+
+    callAPI(nameCity.value, nameCountry.value);
+    //console.log(nameCity.value);
+    //console.log(nameCountry.value);
+})
+
+function callAPI(city, country){
+    const apiId = 'b036a7bc6a67c2e9ff70505dc98917a9';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+
+    fetch(url)
+        .then(data => {
+            return data.json();
+        })
+        .then(dataJSON => {
+            if (dataJSON.cod === '404') {
+                showError('Ciudad no encontrada...');
+            } else {
+                clearHTML();
+                showWeather(dataJSON);
+            }
+            //console.log(dataJSON);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+
+function showWeather(data, datosMeteorologia){
+    const {name, main:{temp, temp_min, temp_max ,humidity , pressure}, weather: [arr], wind: { speed } } = data;
+
+    const degrees = kelvinToCentigrade(temp);
+    const min = kelvinToCentigrade(temp_min);
+    const max = kelvinToCentigrade(temp_max);
+
+    const content = document.createElement('div');
+    content.innerHTML = `
+        <h5>Clima en ${name}</h5>
+        <p>Humedad: ${humidity}%</p>
+        <p>Presión atmosférica: ${pressure} hPa</p>
+        <p>Velocidad del viento: ${speed} m/s</p>
+        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+        <h2>${degrees}°C</h2>
+        <p>Max: ${max}°C</p>
+        <p>Min: ${min}°C</p>
+    `;
+
+    result.appendChild(content);
+}
+
+function showError(message){
+    //console.log(message);
+    const alert = document.createElement('p');
+    alert.classList.add('alert-message');
+    alert.innerHTML = message;
+
+    form.appendChild(alert);
+    setTimeout(() => {
+        alert.remove();
+    }, 3000);
+}
+
+function kelvinToCentigrade(temp){
+    return parseInt(temp - 273.15);
+}
+
+function clearHTML(){
+    result.innerHTML = ''; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const ultimaUbicacion = "Ciudad XYZ"; //
+
+localStorage.setItem('ultimaUbicacion', ultimaUbicacion);
+
+const ubicacionGuardada = localStorage.getItem('ultimaUbicacion');
+console.log(ubicacionGuardada);*/
